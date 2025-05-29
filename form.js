39a60a0a88416dd9c41fa8ff081507070299f9e4
rahulsharma19 <rahul.rahshar@gmail.com>
2025-05-29 11:13:03 +0530

    fix validation on phone number field

--- form.js
+++ form.js
@@ -63,3 +63,15 @@ function formvalidation(){
             $("#successmsg").html(success);
         }
 }       
+jQuery(document).ready(function () {
+  jQuery("#phoneno").keypress(function (e) {
+   var length = jQuery(this).val().length;
+   if(length > 9) {
+      return false;
+   } else if(e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      return false;
+   } else if((length == 0) && (e.which == 48)) {
+      return false;
+   }
+  });
+});
